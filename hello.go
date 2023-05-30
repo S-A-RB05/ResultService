676147274a5a01394df464263e67a986e83d72aa
Sem de Wilde <sem.dewilde6@gmail.com>
2023-05-30 10:20:59 +0200

    Push

--- hello.go
+++ hello.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// Open the HTM file
+	file, err := os.Open("tester_report.htm")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Load the HTM document
+	doc, err := goquery.NewDocumentFromReader(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print all the extracted data from the HTM
+	fmt.Println(doc.Text())
 }
